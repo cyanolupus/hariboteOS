0f974b74289d0f1b9d8bf377d521b685332469ad
cyanolupus <cyanolupus@gmail.com>
2021-03-02 05:37:35 +0900

    Day7-b

--- bootpack.c
+++ bootpack.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include "bootpack.h"
 
+extern struct KEYBUF keybuf;
+
 void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
 	char s[40], mcursor[256];
-	int mx, my;
+	int mx, my, i;
 
 	init_gdtidt();
 	init_pic();
 	io_sti();
+
+	io_out8(PIC0_IMR, 0xf9); /* pic1とキーボードを有効化 */
+	io_out8(PIC1_IMR, 0xef); /* マウスを有効化 */
 
 	init_palette();
 	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
@@ -19,15 +24,20 @@ void HariMain(void)
 	init_mouse_cursor8(mcursor, COL8_008484);
 	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
 	sprintf(s, "(%d,%d)", mx, my);
-	putfonts8_asc(binfo->vram, binfo->scrnx, 9, 9, COL8_000000, "Haribote OS");
-	putfonts8_asc(binfo->vram, binfo->scrnx, 8, 8, COL8_FFFFFF, "Haribote OS");
+	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 100, 10, COL8_FF00FF, "Kawaisou ha kawaii");
-	putfonts8_asc(binfo->vram, binfo->scrnx, 8, 40, COL8_FFFFFF, s);
-
-	io_out8(PIC0_IMR, 0xf9); /* pic1とキーボードを有効化 */
-	io_out8(PIC1_IMR, 0xef); /* マウスを有効化 */
 
 	for (;;) {
-		io_hlt();
+		io_cli();
+		if (keybuf.flag == 0) {
+			io_stihlt();
+		} else {
+			i = keybuf.data;
+			keybuf.flag = 0;
+			io_sti();
+			sprintf(s, "%02X", i);
+			boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
+			putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+		}
 	}
 }
